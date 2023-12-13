9d00a13b7c9fa10d581794c06ccd1f55effc54a8
minhduc65t <minhduc65t@gmail.com>
2023-12-13 11:36:44 +0700

    1

--- app/src/main/java/com/example/do_an_nhom9/MainActivity.java
+++ app/src/main/java/com/example/do_an_nhom9/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.do_an_nhom9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import kotlin.Pair;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,9 +42,16 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image,"logo_image");
+                pairs[1] = new Pair<View,String>(logo,"logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent, options.toBundle());
                 finish();
+
             }
         },SPLASH_SCREEN);
 
